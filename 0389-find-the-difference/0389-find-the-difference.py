3dd235ffce39200b2d20a2346982d1c3bd8ba8b8
Yaseen Asaliya <59315877+yaseen-asaliya@users.noreply.github.com>
2024-12-23 20:10:31 +0200

    Time: 0 ms (100%), Space: 12.6 MB (5.29%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -1,13 +1,7 @@
 class Solution(object):
     def findTheDifference(self, s, t):
-        s = ''.join(sorted(s))
-        t = ''.join(sorted(t))
-        s_len = len(s)
-
-        for index in range(s_len):
-            if t[index] != s[index]:
-                return t[index]
-        
-        return t[s_len]
+        for letter in t:
+            if s.count(letter) != t.count(letter):
+                return letter
             
         
