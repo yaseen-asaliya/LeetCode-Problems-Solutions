0c692add2a2d9ed6c6d071dfbc14af5f5008dae8
Yaseen Asaliya <59315877+yaseen-asaliya@users.noreply.github.com>
2025-02-07 20:04:58 +0200

    Time: 0 ms (100%), Space: 17.7 MB (52.56%) - LeetHub

---
+++ 1450-number-of-students-doing-homework-at-a-given-time/1450-number-of-students-doing-homework-at-a-given-time.py
@@ -0,0 +1,9 @@
+class Solution:
+    def busyStudent(self, startTime: List[int], endTime: List[int], queryTime: int) -> int:
+        count = 0
+
+        for start, end in zip(startTime, endTime):
+            if start <= queryTime <= end:
+                count+= 1
+
+        return count
