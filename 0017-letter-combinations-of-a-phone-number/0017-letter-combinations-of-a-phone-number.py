6f0228111d262820dde77d0eb299bce8aff7161d
Yaseen Asaliya <59315877+yaseen-asaliya@users.noreply.github.com>
2025-05-25 22:38:51 +0300

    Time: 0 ms (100%), Space: 17.9 MB (41.54%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -0,0 +1,30 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        phone_keypad = {
+            '2': 'abc',
+            '3': 'def',
+            '4': 'ghi',
+            '5': 'jkl',
+            '6': 'mno',
+            '7': 'pqrs',
+            '8': 'tuv',
+            '9': 'wxyz'
+        }
+
+        res = []
+
+        if not digits:
+            return res
+
+        def backtrack(index, path):
+            if index == len(digits):
+                res.append(''.join(path))
+                return
+            possible_letters = phone_keypad[digits[index]]
+            for letter in possible_letters:
+                path.append(letter)
+                backtrack(index + 1, path)
+                path.pop()
+
+        backtrack(0, [])
+        return res
