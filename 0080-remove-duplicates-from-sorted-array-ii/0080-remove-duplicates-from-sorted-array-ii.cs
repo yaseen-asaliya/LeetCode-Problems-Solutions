ec7b5a319b9329e3a79fc8ac5aca850f3b5c907b
Yaseen Asaliya <59315877+yaseen-asaliya@users.noreply.github.com>
2025-11-07 20:08:10 +0200

    Time: 149 ms (27.99%), Space: 53 MB (41.49%) - LeetHub

---
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int RemoveDuplicates(int[] nums) {
+        if (nums.Length <= 2) { return nums.Length; }
+
+        var index = 2;
+        var lastValue = nums[0];
+
+        for (int i = 2; i < nums.Length; i++)
+            if (nums[i] != nums[index - 2])
+            {
+                nums[index++] = nums[i];
+                lastValue = nums[index - 2];
+            }
+
+        return index;
+        
+    }
+}
