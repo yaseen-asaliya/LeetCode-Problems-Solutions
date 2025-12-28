134432518348ae021338c18efaea5f6cde16af38
Yaseen Asaliya <59315877+yaseen-asaliya@users.noreply.github.com>
2025-12-28 20:24:31 +0200

    Time: 0 ms (100%), Space: 17.5 MB (90.39%) - LeetHub

---
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.py
@@ -0,0 +1,10 @@
+class Solution:
+    def countSeniors(self, details: List[str]) -> int:
+        c = 0
+
+        for det in details:
+            if int(det[11:13]) > 60:
+                c += 1
+        
+        return c
+        
