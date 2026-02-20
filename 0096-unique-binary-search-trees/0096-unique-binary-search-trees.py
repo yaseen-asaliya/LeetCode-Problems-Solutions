ec31081196b2e86005e569e208570ffbc7fc077a
Yaseen Asaliya <59315877+yaseen-asaliya@users.noreply.github.com>
2026-02-20 21:59:04 +0200

    Time: 0 ms (100%), Space: 19.4 MB (17.7%) - LeetHub

---
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def numTrees(self, n):
+        if n == 0 or n == 1:
+            return 1
+
+        sol = [0] * (n+1)
+        sol[0] = 1
+
+        for i in range(1, n+1):
+            for j in range(i):
+                sol[i] += sol[j] * sol[i-j-1]
+
+        return sol[n]
